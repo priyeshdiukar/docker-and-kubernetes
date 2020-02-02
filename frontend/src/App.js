b29a9d95249476fe63b177f5612a7876fa849fe6
Diukar <g9oohp@splm.siemens.com>
2020-02-02 09:45:15 +0530

    update test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          sign in
         </a>
       </header>
     </div>
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,15 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Test that sigin in link in rendered', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/sign in/i);
   expect(linkElement).toBeInTheDocument();
 });
 
